10de287e9fdbdcae58048cc386416a4e72a5199b
Csaba Hruska <csaba.hruska@gmail.com>
2016-03-21 14:11:09 +0100

    improve java json

--- ddl/out/LambdaCube.TypeInfo2.hpp
+++ ddl/out/LambdaCube.TypeInfo2.hpp
@@ -1,4 +1,5 @@
 // generated file, do not modify!
+<<<<<<< d022971b99be214c71429d58f97f85b13e33a426
 <<<<<<< 4d40c4ee5ed15544d2ac7fb1ea7b61e89766d277
 <<<<<<< c5e063b3c9aeff65161e24445e9672daff980b45
 <<<<<<< 3b50722f433ec5ecc515ea1441fd633d85118cb9
@@ -12,6 +13,9 @@
 =======
 // 2016-03-21T11:36:01.199696000000Z
 >>>>>>> work on java json loader
+=======
+// 2016-03-21T11:41:52.460562000000Z
+>>>>>>> improve java json
 
 #ifndef HEADER_LambdaCube.TypeInfo_H
 #define HEADER_LambdaCube.TypeInfo_H
